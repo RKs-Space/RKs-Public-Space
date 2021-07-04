f5de203e8c736d2ad3597c1fbb8a77ae9df1b660
RK <hi.rahul.listens2u@gmail.com>
2021-07-04 19:37:24 +0530

    Barcode generation Application Initial Push.

---
+++ BarcodeGen Application/Controllers/HomeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using Spire.Xls;
+using IronBarCode;
+
+namespace BarCodeGen.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<SelectListItem> fonts = new List<SelectListItem>();
+        private void setFontfamily()
+        {
+            IList<string> fontNames = FontFamily.Families.Select(f => f.Name).ToList();
+            foreach (var item in fontNames)
+            {
+                fonts.Add(new SelectListItem { Text = item.ToString(), Value = item.ToString() });
+            }
+            ViewData["FontFamilies"] = fonts;
+            List<BarcodeEncoding> codeTypes = Enum.GetValues(typeof(BarcodeEncoding)).Cast<BarcodeEncoding>().ToList();
+            List<string> stringBar = (from o in codeTypes select o.ToString()).ToList();
+            stringBar.Select(m => new SelectListItem { Text = m, Value = m });
+            ViewBag.barTypes = stringBar;
+        }
+        public ActionResult Index()
+        {
+            setFontfamily();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection fc)
+        {
+            setFontfamily();
+            string barType = fc["BarType"];
+            string product = fc["Product"];
+            string barHeight = fc["bHeight"];
+            string barWidth = fc["bWidth"];
+            string fontFamily = fc["fontFamily"];
+            string fontSize = fc["fSize"];
+            
+            //add default value for heigth,width,font,size & make it non mandatory in view
+
+            //List<string> barCodes = 
+                BarGen(barType, barHeight, barWidth, fontFamily, fontSize);
+            //if (barCodes!=null)
+            //{
+            //    ViewData["BarCodesGenerated"] = barCodes;
+            //    ViewBag.BarFeeded = "Ready to Print";
+            //}
+            if (Directory.Exists(Server.MapPath("~/UploadsDirectory")))
+            {
+                Directory.Delete(Server.MapPath("~/UploadsDirectory"), true);
+            }
+            return View("Index");
+        }
+
+
+        [HttpPost]
+        public ActionResult FileUpload(HttpPostedFileBase uploadedFile)
+        {
+            setFontfamily();
+            try
+            {
+                if (uploadedFile.ContentLength>0)
+                {
+                    string szFilename = Path.GetFileName(uploadedFile.FileName);
+                    string szPath = Path.Combine(Server.MapPath("~/UploadsDirectory"), szFilename);
+                    if (!Directory.Exists(szPath))
+                    {
+                        Directory.CreateDirectory(Server.MapPath("~/UploadsDirectory"));
+                    }
+                    uploadedFile.SaveAs(szPath);
+                }
+                ViewBag.UploadSuccess = "Uploaded Successfully";
+                return View("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.UploadError = "File Upload Failed!!";
+                return View("Index");
+            }
+        }
+
+
+        private void BarGen(string barType,string bHeight,string bWidth,string fontFamily,string fSize)
+        {
+            DataTable dtbBarCode = new DataTable();
+            Workbook workbook = new Workbook();
+            Worksheet sheet = null;
+            List<string> barCodesComplete= null;
+            MemoryStream memorystream = null;
+            //Bitmap BarcodeBmp = null;
+            try
+            {
+                if (Directory.Exists(Server.MapPath("~/UploadsDirectory")))
+                {
+                    barCodesComplete = new List<string>();
+                    string[] filePaths = Directory.GetFiles(Server.MapPath("~/UploadsDirectory"));
+                    foreach (var item in filePaths)
+                    {
+                        workbook.LoadFromFile(item, false);
+                        sheet = workbook.Worksheets[0];
+                    }
+                    dtbBarCode = sheet.ExportDataTable();
+                    Bitmap bCode = null;
+                    float fontSize = (float)(Convert.ToInt32(fSize));
+
+                    int iWidth = Convert.ToInt32(bWidth);
+                    int iHeigt = Convert.ToInt32(bHeight);
+
+                    if (barType == string.Empty)
+                    {
+                        barType = "Code39";
+                    }
+                    if (fontFamily == string.Empty)
+                    {
+                        fontFamily = "Arial Black";
+                    }
+                    Font custFont = new Font(new FontFamily(fontFamily), fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
+
+                    //barType.Replace(" ",string.Empty)
+                    BarcodeEncoding bChoosenType = (BarcodeEncoding)Enum.Parse(typeof(BarcodeEncoding), barType,true);
+
+                    foreach (DataRow item in dtbBarCode.Rows)
+                    {
+                        bCode = IronBarCode.BarcodeWriter.CreateBarcode(item.ItemArray[0].ToString(), bChoosenType).ResizeTo(iWidth, iHeigt).AddBarcodeValueTextBelowBarcode(custFont, Color.Black).SetMargins(2).ToBitmap();
+                        if (bCode != null)
+                        {
+                            memorystream = new MemoryStream();
+                            bCode.Save(memorystream, ImageFormat.Png);
+                            barCodesComplete.Add("data:image/png;base64," + Convert.ToBase64String(memorystream.ToArray()));
+                        }
+                    }
+                    ViewData["BarCodesGenerated"] = barCodesComplete;
+                    ViewBag.BarFeeded = "Ready to Print";
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ExceptionMsg = "Incorrect Data for Barcode Type";
+            }
+            //return barCodesComplete;
+        }
+
+    }
+}
---
+++ BarcodeGen Application/Controllers/SingleBarController.cs
@@ -0,0 +1,59 @@
+﻿using IronBarCode;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BarGen.Controllers
+{
+    public class SingleBarController : Controller
+    {
+
+        private void setBarcodeTypes()
+        {
+            List<BarcodeEncoding> codeTypes = Enum.GetValues(typeof(BarcodeEncoding)).Cast<BarcodeEncoding>().ToList();
+            List<string> stringBar = (from o in codeTypes select o.ToString()).ToList();
+            stringBar.Select(m => new SelectListItem { Text = m, Value = m });
+            ViewBag.barTypes = stringBar;
+        }
+        // GET: SingleBar
+        public ActionResult SingleBarView()
+        {
+            setBarcodeTypes();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SingleBarView(FormCollection fc)
+        {
+            setBarcodeTypes();
+            string value = fc["BarType"];
+            string product = fc["Product"];
+            MemoryStream memorystream = new MemoryStream();
+            Bitmap BarcodeBmp = null;
+
+            try
+            {
+                Font custFont = new Font(new FontFamily("Arial Black"), 15f, FontStyle.Regular, GraphicsUnit.Pixel);
+                BarcodeEncoding bChoosenType = (BarcodeEncoding)Enum.Parse(typeof(BarcodeEncoding), value, true);
+
+                BarcodeBmp = IronBarCode.BarcodeWriter.CreateBarcode(product, bChoosenType).ResizeTo(300, 100).AddBarcodeValueTextBelowBarcode(custFont, Color.Black).SetMargins(5).ToBitmap();
+                if (!string.IsNullOrEmpty(value) && !string.IsNullOrEmpty(product))
+                {
+                    BarcodeBmp.Save(memorystream, ImageFormat.Png);
+                    ViewBag.BarcodeImage = "data:image/png;base64," + Convert.ToBase64String(memorystream.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ExceptionMsg = "Incorrect Data for Barcode Type";
+            }
+            
+            return View("SingleBarView");
+        }
+    }
+}
