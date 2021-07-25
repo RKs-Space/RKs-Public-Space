509fc30f4eeefa6a05ca23a012ba2ee921fa54f5
RK <hi.rahul.listens2u@gmail.com>
2021-07-25 07:06:05 +0530

    Server Master App initial Commit
    This is a cool Windows application to help you navigate to the Server group for Monitoring purposes directly without having to open individual Server groups

---
+++ Server Master Application/AppUi.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Server_Master
+{
+    partial class AppUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ddEnv = new System.Windows.Forms.ComboBox();
+            this.ddServer = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.lblException = new System.Windows.Forms.Label();
+            this.btnOpenServer = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.SuspendLayout();
+            // 
+            // ddEnv
+            // 
+            this.ddEnv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ddEnv.FormattingEnabled = true;
+            this.ddEnv.Items.AddRange(new object[] {
+            "Production",
+            "Stage"});
+            this.ddEnv.Location = new System.Drawing.Point(139, 109);
+            this.ddEnv.Name = "ddEnv";
+            this.ddEnv.Size = new System.Drawing.Size(135, 21);
+            this.ddEnv.TabIndex = 0;
+            this.ddEnv.SelectionChangeCommitted += new System.EventHandler(this.ddEnv_SelectionChangeCommitted);
+            // 
+            // ddServer
+            // 
+            this.ddServer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ddServer.FormattingEnabled = true;
+            this.ddServer.Location = new System.Drawing.Point(139, 182);
+            this.ddServer.Name = "ddServer";
+            this.ddServer.Size = new System.Drawing.Size(121, 21);
+            this.ddServer.TabIndex = 1;
+            this.ddServer.SelectedIndexChanged += new System.EventHandler(this.ddServer_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(137, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Environment";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(137, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Server Group";
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblServer.Location = new System.Drawing.Point(137, 225);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(51, 20);
+            this.lblServer.TabIndex = 4;
+            this.lblServer.Text = "label3";
+            // 
+            // lblException
+            // 
+            this.lblException.AutoSize = true;
+            this.lblException.BackColor = System.Drawing.Color.Transparent;
+            this.lblException.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblException.ForeColor = System.Drawing.Color.Red;
+            this.lblException.Location = new System.Drawing.Point(225, 225);
+            this.lblException.Name = "lblException";
+            this.lblException.Size = new System.Drawing.Size(51, 20);
+            this.lblException.TabIndex = 6;
+            this.lblException.Text = "label3";
+            // 
+            // btnOpenServer
+            // 
+            this.btnOpenServer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOpenServer.Depth = 0;
+            this.btnOpenServer.Location = new System.Drawing.Point(139, 265);
+            this.btnOpenServer.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnOpenServer.Name = "btnOpenServer";
+            this.btnOpenServer.Primary = true;
+            this.btnOpenServer.Size = new System.Drawing.Size(114, 38);
+            this.btnOpenServer.TabIndex = 8;
+            this.btnOpenServer.Text = "Open Servers";
+            this.btnOpenServer.UseVisualStyleBackColor = true;
+            this.btnOpenServer.Click += new System.EventHandler(this.btnOpenServer_Click);
+            // 
+            // AppUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 325);
+            this.Controls.Add(this.btnOpenServer);
+            this.Controls.Add(this.lblException);
+            this.Controls.Add(this.lblServer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ddServer);
+            this.Controls.Add(this.ddEnv);
+            this.Name = "AppUi";
+            this.Text = "Server Master";
+            this.Load += new System.EventHandler(this.AppUi_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ddEnv;
+        private System.Windows.Forms.ComboBox ddServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.Label lblException;
+        private MaterialSkin.Controls.MaterialRaisedButton btnOpenServer;
+    }
+}
+
---
+++ Server Master Application/AppUi.cs
@@ -0,0 +1,142 @@
+﻿using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_Master
+{
+    public partial class AppUi : MaterialSkin.Controls.MaterialForm
+    {
+        public AppUi()
+        {
+            InitializeComponent();
+            MaterialSkin.MaterialSkinManager skinMnager = MaterialSkin.MaterialSkinManager.Instance;
+            SkinManager.AddFormToManage(this);
+            SkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.DARK;
+            SkinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Green900, MaterialSkin.Primary.BlueGrey900, MaterialSkin.Primary.BlueGrey500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
+        }
+
+        public DataTable dtbProdServerList = new DataTable();
+        public DataTable dtbStageServerList = new DataTable();
+        public Dictionary<string, string> dcProdServerList = new Dictionary<string, string>();
+        public Dictionary<string, string> dcStageServerList = new Dictionary<string, string>();
+
+
+        private void AppUi_Load(object sender, EventArgs e)
+        {
+            lblException.Visible = false;
+            lblServer.Visible = false;
+            ddServer.Enabled = false;
+            btnOpenServer.Enabled = false;
+            setServerList();
+        }
+
+        private void ddEnv_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            lblServer.Visible = false;
+            if (ddEnv.SelectedItem.ToString().Trim() == "Production")
+            {
+                ddServer.Enabled = true;
+                btnOpenServer.Enabled = true;
+                ddServer.DataSource = dtbProdServerList;
+                ddServer.ValueMember = "Server Name";
+                ddServer.DisplayMember = "Server Name";
+            }
+            if (ddEnv.SelectedItem.ToString().Trim() == "Stage")
+            {
+                ddServer.Enabled = true;
+                btnOpenServer.Enabled = true;
+                ddServer.DataSource = dtbStageServerList;
+                ddServer.ValueMember = "Server Name";
+                ddServer.DisplayMember = "Server Name";
+            }
+        }
+        
+
+        public void setServerList()
+        {
+            string szServerListPath = Convert.ToString(ConfigurationManager.AppSettings["serverListPath"]).Trim();
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(szServerListPath);
+            Worksheet sheetProd = workbook.Worksheets[0];
+            dtbProdServerList = sheetProd.ExportDataTable();
+            Worksheet sheetStage = workbook.Worksheets[1];
+            dtbStageServerList = sheetStage.ExportDataTable();
+            dcProdServerList = dtbProdServerList.AsEnumerable().ToDictionary(row => row.Field<string>(0).Trim(), row => row.Field<string>(1).Trim());
+            dcStageServerList = dtbStageServerList.AsEnumerable().ToDictionary(row => row.Field<string>(0).Trim(), row => row.Field<string>(1).Trim());
+        }
+
+        private void ddServer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] szValue = new string[5];
+            lblException.Visible = false;
+            if (ddEnv.Text.Trim() == "Production")
+            {
+                if (dcProdServerList.ContainsKey(Convert.ToString(ddServer.Text).Trim()))
+                {
+
+                    lblServer.Text = dcProdServerList[ddServer.Text.Trim()];
+                    if (lblServer.Text.Length > 17)
+                    {
+                        szValue = Convert.ToString(lblServer.Text).Split(' ');
+                        lblServer.Text = szValue[0] + "\n" + szValue[1] + "\n" + szValue[2];
+                    }
+                    lblServer.Visible = true;
+                }
+            }
+            if (ddEnv.Text.Trim() == "Stage")
+            {
+                if (dcStageServerList.ContainsKey(Convert.ToString(ddServer.Text).Trim()))
+                {
+
+                    lblServer.Text = dcStageServerList[ddServer.Text.Trim()];
+                    if (lblServer.Text.Length > 17)
+                    {
+                        szValue = Convert.ToString(lblServer.Text).Split(' ');
+                        lblServer.Text = szValue[0] + "\n" + szValue[1] + "\n" + szValue[2];
+                    }
+                    lblServer.Visible = true;
+                }
+            }
+        }
+
+        private void btnOpenServer_Click(object sender, EventArgs e)
+        {
+            string szServerPath = string.Empty;
+            szServerPath = Convert.ToString(ConfigurationManager.AppSettings["serverPath"]).Trim();
+            try
+            {
+                if (ddEnv.Text.Trim() == "Production")
+                {
+                    if (dcProdServerList.ContainsKey(Convert.ToString(ddServer.Text).Trim()))
+                    {
+                        szServerPath = szServerPath + @"Production\" + Convert.ToString(dcProdServerList.FirstOrDefault(x => x.Value.Equals(dcProdServerList[Convert.ToString(ddServer.Text).Trim()])).Key) + ".msc";
+                        Process.Start(szServerPath);
+                    }
+                }
+                if (ddEnv.Text.Trim() == "Stage")
+                {
+                    if (dcStageServerList.ContainsKey(Convert.ToString(ddServer.Text).Trim()))
+                    {
+                        szServerPath = szServerPath + @"Stage\" + Convert.ToString(dcStageServerList.FirstOrDefault(x => x.Value.Equals(dcStageServerList[Convert.ToString(ddServer.Text).Trim()])).Key) + ".msc";
+                        Process.Start(szServerPath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblServer.Visible = false;
+                lblException.Text = "Unable to Open Server!! Error- Please Try Again";
+                lblException.Visible = true;
+            }
+        }
+    }
+}
